eca37dd0013113a130bf248d873e3c4aa3a23014
qwerty326 <34879827+qwerty326@users.noreply.github.com>
2019-05-24 09:25:50 +0800

    Add files via upload

---
+++ demoDlg.cpp
@@ -0,0 +1,210 @@
+
+// demoDlg.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "demo.h"
+#include "demoDlg.h"
+#include "afxdialogex.h"
+#include "CvvImage.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+CvCapture* capture;
+CRect rect;
+CDC *pDC;
+HDC hDC;
+CWnd *pwnd;
+
+// 用于应用程序“关于”菜单项的 CAboutDlg 对话框
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 对话框数据
+	enum { IDD = IDD_ABOUTBOX };
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 支持
+
+// 实现
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(CAboutDlg::IDD)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+END_MESSAGE_MAP()
+
+
+// CdemoDlg 对话框
+
+
+
+CdemoDlg::CdemoDlg(CWnd* pParent /*=NULL*/)
+	: CDialogEx(CdemoDlg::IDD, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CdemoDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CdemoDlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON1, &CdemoDlg::OnBnClickedButton1)
+	ON_WM_TIMER()
+	ON_BN_CLICKED(IDC_BUTTON2, &CdemoDlg::OnBnClickedButton2)
+END_MESSAGE_MAP()
+
+
+// CdemoDlg 消息处理程序
+
+BOOL CdemoDlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 将“关于...”菜单项添加到系统菜单中。
+
+	// IDM_ABOUTBOX 必须在系统命令范围内。
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != NULL)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO:  在此添加额外的初始化代码
+	pwnd = GetDlgItem(IDC_STATIC);
+	pDC = pwnd->GetDC();
+	hDC = pDC->GetSafeHdc();
+
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+
+void CdemoDlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CdemoDlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CdemoDlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CdemoDlg::OnBnClickedButton1()
+{
+	// 读取摄像头数据
+	if (!capture)
+	{
+		capture = cvCaptureFromCAM(0);
+	}
+
+	if (!capture)
+	{
+		AfxMessageBox(_T("无法打开摄像头"));
+		return;
+	}
+
+	// 设置计时器,每10ms触发一次事件
+	SetTimer(1, 10, NULL);
+
+}
+
+
+void CdemoDlg::OnTimer(UINT_PTR nIDEvent)
+{
+	// 循环显示图像帧
+	IplImage* m_Frame;
+	m_Frame = cvQueryFrame(capture);
+	CvvImage m_CvvImage;
+	m_CvvImage.CopyOf(m_Frame, 1);
+
+	//窗口大小匹配图像分辨率
+	GetDlgItem(IDC_STATIC)->GetClientRect(&rect);
+	GetDlgItem(IDC_STATIC)->SetWindowPos(NULL, 0, 0, m_Frame->width, m_Frame->height, SWP_NOZORDER | SWP_NOMOVE);
+	m_CvvImage.DrawToHDC(hDC, &rect);
+
+	//CDialogEx::OnTimer(nIDEvent);
+}
+
+
+void CdemoDlg::OnBnClickedButton2()
+{
+	// 停止读取摄像头
+	KillTimer(1);
+	Invalidate();
+	cvReleaseCapture(&capture);
+}
